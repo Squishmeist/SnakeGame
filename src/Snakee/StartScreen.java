01824d3d26eef10f8507b32d03b8484e34146cce
Ainsley Lee <psyal5@nottingham.ac.uk>
2022-11-10 22:58:10 +0000

    StartScreen feat : Expanded upon StartScreen class adding a game title, as well as a textfield for players to input their name.
     A start button has also been added and displays a message when clicked. This will be used in the future to allow the player to start the game.

--- src/Snakee/StartScreen.java
+++ src/Snakee/StartScreen.java
@@ -1,32 +1,82 @@
 package Snakee;
 
 import javafx.application.Application;
-import javafx.scene.image.Image;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
+import javafx.scene.text.Text;
+import javafx.scene.image.Image;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
+
 
 public class StartScreen extends Application {
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) {
+        GridPane grid = new GridPane();
+
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Snakee");
+        scenetitle.setId("title-text");
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label playerNameLabel = new Label("Player Name:");
+        grid.add(playerNameLabel, 0, 1);
+
+        TextField playerNameField = new TextField();
+        grid.add(playerNameField, 1, 1);
+
+        //BUTTON CREATED
+        Button startButton = new Button("Start");
+        HBox hbBtn = new HBox(10);
+
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(startButton);
+
+        grid.add(hbBtn, 1, 4);
+
+        //BUTTON EVENT HANDLER DISPLAY MESSAGE
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+        actiontarget.setId("actiontarget");
+
+        //BUTTON EVENT HANDLER
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Start Button Pressed");
+            }
+        });
+
+
+
+        //Creates a scene object
+        Scene scene = new Scene(grid, 870, 560);
+        //Sets stage title
+        stage.setTitle("Snake Yipee");
+        //Sets stage icon
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/snake-logo.png")));
+        //Adds scene to the stage
+        stage.setScene(scene);
+
+        scene.getStylesheets().add
+                (StartScreen.class.getResource("Main.css").toExternalForm());
+
+        //Displays the contents of the stage
+        stage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        root.setId("start-pane");
-
-        Scene scene = new Scene(root, 870, 560);
-        scene.getStylesheets().add(String.valueOf(this.getClass().getResource("Main.css")));
-
-
-        primaryStage.setTitle("Snakee Yipee");
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("images/snake-logo.png")));
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
