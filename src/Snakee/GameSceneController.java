f394dd117d90f056755c4d0aef053bfcf75fd681
Ainsley Lee <psyal5@nottingham.ac.uk>
2022-11-11 17:23:04 +0000

    Snake move feat :
    Using SceneBuilder a circle object with the ID SnakeHeadCircle was added to the scene.
    Through the keyPressed method in the GameSceneController class I used the setCenterX and Y method to increase or decrease the x and y of the circle object depending on the keys pressed.
    
    This allows the circle object to move around the scene smoothly. I need to develop this further and put checks in place to ensure the snake object remains in the scene dimentions.

--- src/Snakee/GameSceneController.java
+++ src/Snakee/GameSceneController.java
@@ -9,8 +9,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
 import java.io.IOException;
 
 public class GameSceneController {
@@ -20,9 +22,36 @@ public class GameSceneController {
 
     @FXML
     Label playernameLabel;
+    @FXML
+    private Circle SnakeHeadCircle;
+    private double x;
+    private double y;
 
     public void playerName(String playerName) {
         playernameLabel.setText("PLAYER : " + playerName);
+    }
+
+    @FXML
+    public void keyPressed(KeyEvent event){
+        if(event.getCode() == KeyCode.W){
+            System.out.println("Pressed W");
+            SnakeHeadCircle.setCenterY(y-=5);
+        }
+
+        if(event.getCode() == KeyCode.S){
+            System.out.println("Pressed S");
+            SnakeHeadCircle.setCenterY(y+=5);
+        }
+
+        if(event.getCode() == KeyCode.A){
+            System.out.println("Pressed A");
+            SnakeHeadCircle.setCenterX(x-=5);
+        }
+
+        if(event.getCode() == KeyCode.D){
+            System.out.println("Pressed D");
+            SnakeHeadCircle.setCenterX(x+=5);
+        }
     }
 
     public void switchToStartScene(ActionEvent event) throws IOException {
@@ -31,28 +60,5 @@ public class GameSceneController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-    }
-
-
-    @FXML
-    public void keyPressed(KeyEvent event){
-        if(event.getCode() == KeyCode.W){
-            System.out.println("Pressed W");
-        }
-
-        if(event.getCode() == KeyCode.S){
-            System.out.println("Pressed S");
-        }
-
-        if(event.getCode() == KeyCode.A){
-            System.out.println("Pressed A");
-        }
-
-        if(event.getCode() == KeyCode.D){
-            System.out.println("Pressed D");
-        }
-
-
-
     }
 }
