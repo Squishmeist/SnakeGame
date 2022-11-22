93e2999d218f53509fb2051f270d02a82325e425
Ainsley Lee <psyal5@nottingham.ac.uk>
2022-11-22 18:23:39 +0000

    Food Fix : different food images generated
    A random number is created and passed to the GenerateFoodImage method.
    This takes the given number and using switch cases sets the foodImage to
     the corresponding image of the given number.
    
    The foodImage is returned back from the method and used to setFill the
    foodObject.

--- src/Snakee/GameSceneController.java
+++ src/Snakee/GameSceneController.java
@@ -218,20 +218,88 @@ public class GameSceneController implements Initializable{
         snakeTail.setTranslateY(y);
     }
 
-    private void FoodGenerate(){
+    private Image GenerateFoodImage(int foodImageNumber, Image foodImage){
+        switch (foodImageNumber) {
+            case 1:
+                foodImage = new Image("Snakee/images/food-apple.png");
+                break;
+            case 2:
+                foodImage = new Image("Snakee/images/food-banana.png");
+                break;
+            case 3:
+                foodImage = new Image("Snakee/images/food-banana.png");
+                break;
+            case 4:
+                foodImage = new Image("Snakee/images/food-blueberry.png");
+                break;
+            case 5:
+                foodImage = new Image("Snakee/images/food-cherry.png");
+                break;
+            case 6:
+                foodImage = new Image("Snakee/images/food-durian.png");
+                break;
+            case 7:
+                foodImage = new Image("Snakee/images/food-grape.png");
+                break;
+            case 8:
+                foodImage = new Image("Snakee/images/food-grapefruit.png");
+                break;
+            case 9:
+                foodImage = new Image("Snakee/images/food-kiwi.png");
+                break;
+            case 10:
+                foodImage = new Image("Snakee/images/food-lemon.png");
+                break;
+            case 11:
+                foodImage = new Image("Snakee/images/food-litchi.png");
+                break;
+            case 12:
+                foodImage = new Image("Snakee/images/food-mango.png");
+                break;
+            case 13:
+                foodImage = new Image("Snakee/images/food-orange.png");
+                break;
+            case 14:
+                foodImage = new Image("Snakee/images/food-peach.png");
+                break;
+            case 15:
+                foodImage = new Image("Snakee/images/food-pear.png");
+                break;
+            case 16:
+                foodImage = new Image("Snakee/images/food-pineapple.png");
+                break;
+            case 17:
+                foodImage = new Image("Snakee/images/food-pitaya.png");
+                break;
+            case 18:
+                foodImage = new Image("Snakee/images/food-strawberry.png");
+                break;
+            case 19:
+                foodImage = new Image("Snakee/images/food-watermelon.png");
+                break;
+            default:
+                // code block
+        }
+        return foodImage;
+    }
+
+    private void FoodGenerate() {
         //Generates random x and y points for food to spawn
-        int xFood = (int)(Math.random() * (860) + 0 );
-        int yFood = (int)(Math.random() * (550) + 0 );
+        int foodX = (int) (Math.random() * (860) + 0);
+        int foodY = (int) (Math.random() * (550) + 0);
+        int foodImageNumber = (int) (Math.random() * (19) + 0);
+
+        Image foodImage = null;
 
         //Sets food objects x and y to randomly generated number
-        foodObject.setX(xFood);
-        foodObject.setY(yFood);
+        foodObject.setX(foodX);
+        foodObject.setY(foodY);
         //Sets food objects size
         foodObject.setWidth(snakeSize);
         foodObject.setHeight(snakeSize);
 
+        foodImage = GenerateFoodImage(foodImageNumber, foodImage);
         //Loads image to fill rectangle object
-        Image foodImage = new Image("Snakee/images/food-apple.png");
         foodObject.setFill(new ImagePattern(foodImage));
 
         //Sets food exists to equal true
