42cd24a34fe97e94e57c54d1b46d044684e0a006
psyal5 <psyal5@nottingham.ac.uk>
2022-12-08 20:39:07 +0000

    Maven feat : sourcecode example
    Maven build working with example code.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.siebers.pathdemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.Snake to javafx.fxml;
+    exports com.Snake;
+    exports com.Snake.controller;
+    opens com.Snake.controller to javafx.fxml;
+    exports com.Snake.model;
+    opens com.Snake.model to javafx.fxml;
+}
